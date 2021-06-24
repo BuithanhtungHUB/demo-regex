249d098d5cd1407a5fbc05726c637e7397cf7095
BuithanhtungHUB <83797698+BuithanhtungHUB@users.noreply.github.com>
2021-06-24 15:34:00 +0700

    Update Validate.php

--- Validate.php
+++ Validate.php
@@ -5,7 +5,7 @@ class Validate
 {
     public static function check_username($username)
     {
-        $pattern = "/^[A-Za-z][]{6,}$/";
+        $pattern = "/^[A-Za-z]{6,}$/";
         if (!preg_match($pattern,$username)){
             session_start();
             $_SESSION['username']="Username sai roi";
